7cbab850b22556249c05ae0a3bee926daa1b40b8
akannur <akannur@qwinixtech.com>
2015-08-07 09:33:18 +0530

    Display only images

--- jquery.jstwitter.js
+++ jquery.jstwitter.js
@@ -58,17 +58,19 @@ $(function() {
 	                    try {
 	                      if (data[i].entities['media']) {
 	                        img = '<a href="' + url + '" target="_blank"><img src="' + data[i].entities['media'][0].media_url + '" /></a>';
-	                      }
-	                    } catch (e) {  
-	                      //no media
-	                    }
-	                  
-	                    $(JQTWEET.appendTo).append( JQTWEET.template.replace('{TEXT}', JQTWEET.ify.clean(data[i].text) )
+
+	                        $(JQTWEET.appendTo).append( JQTWEET.template.replace('{TEXT}', JQTWEET.ify.clean(data[i].text) )
 	                        .replace('{USER}', data[i].user.screen_name)
 	                        .replace('{IMG}', img)                                
 	                        .replace('{AGO}', JQTWEET.timeAgo(data[i].created_at) )
 	                        .replace('{URL}', url )			                            
 	                        );
+	                      }
+	                    } catch (e) {  
+	                      //no media
+	                    }
+	                  
+	                    
 	                  }
                   
                   } catch (e) {
